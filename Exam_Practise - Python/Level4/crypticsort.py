99d0f3753bacab7542e491913109addf4007d831
Parid <ridi5973@gmail.com>
2026-03-08 20:53:37 +0100

    added exercise

--- Exam_Practise - Python/Level4/crypticsort.py
+++ Exam_Practise - Python/Level4/crypticsort.py
@@ -1,1 +1,7 @@
-# Find it Chat lol dont have prototype yet
+#not confirmed solution
+import sys
+
+if len(sys.argv) == 2:
+    word = sys.argv[1]
+    sorted_word = sorted(word)
+    print("".join(sorted_word))
