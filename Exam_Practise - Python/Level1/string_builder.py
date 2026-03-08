14350fefbf41b4688e0d63fcc5b50dc80995bcbd
Parid <ridigjerasi@gmail.com>
2026-03-08 03:59:24 +0100

    string builder case upper lower

--- Exam_Practise - Python/Level1/string_builder.py
+++ Exam_Practise - Python/Level1/string_builder.py
@@ -1,1 +1,16 @@
 # string_builder and it required to alterate the string from lowercase to uppercase character by character
+
+
+name = "BERTI" #ifykyk ;))
+
+def string(name:str)-> str:
+    result = []
+    for char in name:
+        if char.islower():
+         char= char.upper()
+        elif char.isupper():
+            char = char.lower()
+        result.append(char)
+    return "" .join(result)
+
+print(string(name))
